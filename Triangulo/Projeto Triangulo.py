09d294c1dbdfd6395418f948aca7a2b968e91517
maciel-ana <130106962+maciel-ana@users.noreply.github.com>
2023-10-16 11:07:40 -0300

    Commit Triangulo

--- Triangulo/Projeto Triangulo.py
+++ Triangulo/Projeto Triangulo.py
@@ -1,3 +1,6 @@
+
+print('\n\t\t --- Validador de Triangulo --- \n\t\t')
+#Entradas
 la = float(input('Digite o Valor do Lado A: ').strip())
 lb = float(input('Digite o Valor do Lado B: ').strip())
 lc = float(input('Digite o Valor do Lado C: ').strip())
