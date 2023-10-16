7d5e5582c9433de3687b50d5f2c7e705b3d9f76a
maciel-ana <130106962+maciel-ana@users.noreply.github.com>
2023-10-16 11:03:24 -0300

    Primeiro Commit

---
+++ Triangulo/Projeto Triangulo.py
@@ -0,0 +1,11 @@
+la = float(input('Digite o Valor do Lado A: ').strip())
+lb = float(input('Digite o Valor do Lado B: ').strip())
+lc = float(input('Digite o Valor do Lado C: ').strip())
+
+lp = (lb - lc) < la < lb + lc
+
+if lp == True:
+    print('É um Triangulo Válido!')
+
+else:
+    print('Não é um Triangulo válido')
