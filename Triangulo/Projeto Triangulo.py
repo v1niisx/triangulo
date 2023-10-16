3447aec9fdf3a0589e38331deb8b49ac84f4123e
maciel-ana <130106962+maciel-ana@users.noreply.github.com>
2023-10-16 11:43:04 -0300

    Commit Triangulo

--- Triangulo/Projeto Triangulo.py
+++ Triangulo/Projeto Triangulo.py
@@ -1,10 +1,10 @@
-
+#Validar Triangulo
 print('\n\t\t --- Validador de Triangulo --- \n\t\t')
 #Entradas
 la = float(input('Digite o Valor do Lado A: ').strip())
 lb = float(input('Digite o Valor do Lado B: ').strip())
 lc = float(input('Digite o Valor do Lado C: ').strip())
-
+#Processamento e Saida
 lp = la < (lb+lc) and lb < (la+lc) and lc < (la + lb)
 
 if lp == True:
